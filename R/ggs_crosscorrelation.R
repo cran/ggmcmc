e50b98fc6e64c84bcddd85c1822ae65861b131bb
Xavier Fernández i Marín <xavier.fim@gmail.com>
2013-07-05 00:00:00 +0000

    version 0.4

--- R/ggs_crosscorrelation.R
+++ R/ggs_crosscorrelation.R
@@ -4,13 +4,13 @@
 #'
 #' @param D Data frame whith the simulations.
 #' @param family Name of the family of parameters to plot, as given by a character vector or a regular expression. A family of parameters is considered to be any group of parameters with the same name but different numerical value between square brackets (as beta[1], beta[2], etc). 
-#' @param absolute.scale Logical. When TRUE (the default), the scale of the colour diverges between perfect inverse correlation (-1) to perfect correlation (1), whereas when FALSE, the scale is relative to the minimum and maximum cross-correlations observed.
+#' @param absolute_scale Logical. When TRUE (the default), the scale of the colour diverges between perfect inverse correlation (-1) to perfect correlation (1), whereas when FALSE, the scale is relative to the minimum and maximum cross-correlations observed.
 #' @return a \code{ggplot} object.
 #' @export
 #' @examples
 #' data(samples)
-#' ggs_crosscorrelation(ggs(S, parallel=FALSE))
-ggs_crosscorrelation <- function(D, family=NA, absolute.scale=TRUE) {
+#' ggs_crosscorrelation(ggs(S))
+ggs_crosscorrelation <- function(D, family=NA, absolute_scale=TRUE) {
   # Manage subsetting a family of parameters
   if (!is.na(family)) {
     D <- get_family(D, family=family)
@@ -18,18 +18,18 @@ ggs_crosscorrelation <- function(D, family=NA, absolute.scale=TRUE) {
   if (attributes(D)$nParameters <= 1) {
     stop("Can't calculate crosscorrelations with a single chain")
   } 
-  X <- cast(D, Iteration + Chain ~ Parameter)
+  X <- dcast(D, Iteration + Chain ~ Parameter)
   # Chain management is not easy
-  bc.cc <- melt(cor(as.matrix(X[,-c(1, 2)])))
+  bc.cc <- melt(cor(as.matrix(X[,-c(1, 2), drop=FALSE])))
   # Need to revert parameter names
-  bc.cc$X1 <- factor(bc.cc$X1, labels=levels(D$Parameter))
-  bc.cc$X2 <- factor(bc.cc$X2, labels=levels(D$Parameter))
-  bc.cc$value[bc.cc$X1==bc.cc$X2] <- NA
+  bc.cc$Var1 <- factor(bc.cc$Var1, labels=levels(D$Parameter))
+  bc.cc$Var2 <- factor(bc.cc$Var2, labels=levels(D$Parameter))
+  bc.cc$value[bc.cc$Var1==bc.cc$Var2] <- NA
   # Plot
-  f <- ggplot(bc.cc, aes(x=X1, y=X2)) + 
+  f <- ggplot(bc.cc, aes(x=Var1, y=Var2)) +
     geom_tile(aes(fill=value)) +
     xlab("") + ylab("") + theme(axis.text.x=element_text(angle=90))
-    if (absolute.scale) {
+    if (absolute_scale) {
       f <- f + scale_fill_gradient2(limits=c(-1, 1)) 
     } else {
       f <- f + scale_fill_gradient2() 
